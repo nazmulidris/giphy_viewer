40f9ae8c5479e7e30ddc04c62e0cbfaa64a42cff
Nazmul Idris <nazmul@fasterlap.com>
2018-12-14 17:47:05 -0800

    Add Giphy SDK and update copyright

--- app/src/main/java/com/nazmul/giphy_viewer/AppViewModel.java
+++ app/src/main/java/com/nazmul/giphy_viewer/AppViewModel.java
@@ -1,12 +1,31 @@
+/*
+ * Copyright 2018 Nazmul Idris. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nazmul.giphy_viewer;
 
 import android.app.Application;
 import android.os.Handler;
 import android.util.Log;
 
+import com.giphy.sdk.core.models.Media;
+
 import org.greenrobot.eventbus.EventBus;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -44,6 +63,12 @@ public class AppViewModel extends AndroidViewModel {
         if (runOnRefreshComplete != null) {
             new Handler(getApplication().getMainLooper()).postDelayed(runOnRefreshComplete, 1000);
         }
+
+        GiphyClient.makeTrendingRequest(
+                null,
+                (List<Media> mediaList) -> {
+                    Log.d("logtag", "requestDataRefresh mediaList.size: " + mediaList.size());
+                });
     }
 
     // TODO This needs to be wired to a overscroll detection event and be called.
--- app/src/main/java/com/nazmul/giphy_viewer/GiphyClient.java
+++ app/src/main/java/com/nazmul/giphy_viewer/GiphyClient.java
@@ -1,5 +1,76 @@
+/*
+ * Copyright 2018 Nazmul Idris. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nazmul.giphy_viewer;
 
-public class GiphyClient {
+import android.os.Looper;
+import android.util.Log;
 
+import com.giphy.sdk.core.models.Media;
+import com.giphy.sdk.core.models.enums.MediaType;
+import com.giphy.sdk.core.models.enums.RatingType;
+import com.giphy.sdk.core.network.api.CompletionHandler;
+import com.giphy.sdk.core.network.api.GPHApi;
+import com.giphy.sdk.core.network.api.GPHApiClient;
+import com.giphy.sdk.core.network.response.ListMediaResponse;
+import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class GiphyClient {
+    public static final String API_KEY = "mnVttajnx9Twmgp3vFbMQa3Gvn9Rv4Hg";
+    public static final GPHApi client = new GPHApiClient(API_KEY);
+
+    /**
+     * @param runOnComplete This Runnable will be executed after the API response is received. If
+     *     the response is an error or contains results, this will be run. This is a good place to
+     *     put UI code that changes the state of any components that are "waiting". This code is run
+     *     on the main thread.
+     * @param onResponseHandler The <code>onResponse</code> method is called only if results are
+     *     returned in the API response.
+     */
+    public static void makeTrendingRequest(
+            @Nullable Runnable runOnComplete, @NonNull GiphyResultsHandler onResponseHandler) {
+
+        CompletionHandler<ListMediaResponse> completionHandler =
+                (results, exception) -> {
+                    if (Looper.myLooper() == Looper.getMainLooper()) {
+                        Log.d("logtag", "makeTrendingRequest running in main thread");
+                    } else {
+                        Log.d("logtag", "🛑 makeTrendingRequest NOT running in main thread");
+                    }
+
+                    if (results != null && results.getData() != null) {
+                        for (Media gif : results.getData()) {
+                            Log.d("logtag", "gif.getId(): " + gif.getId());
+                        }
+                        onResponseHandler.onResponse(results.getData());
+                    }
+                    if (runOnComplete != null) runOnComplete.run();
+                };
+
+        client.trending(
+                /* type= */ MediaType.gif,
+                /* limit= */ null,
+                /* offset= */ null,
+                /* rating */ RatingType.g,
+                /* completionHandler */ completionHandler);
+    }
+
+    public interface GiphyResultsHandler {
+        void onResponse(List<Media> mediaList);
+    }
 }
